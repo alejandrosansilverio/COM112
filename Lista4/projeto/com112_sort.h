ea93ad4d5cd7839cb4534e526e04e92c7df028c0
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2020-05-08 01:54:19 -0300

    Update com112_sort.h

--- Lista4/projeto/com112_sort.h
+++ Lista4/projeto/com112_sort.h
@@ -1,1 +1,8 @@
+#ifndef COM_112SORT_H
+#define COM_112SORT_H
 
+void bubble_sort(int vetor[], int n);//método de ordenação bubble sort
+void selection_Sort(int vet[],int n);//método de ordenação selection sort
+void insertion_sort(int vetor[], int n);//método de ordenação insertion sort
+
+#endif
