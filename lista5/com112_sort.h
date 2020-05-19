f81c0b084828bd98733497fb0491ba41cef4e13a
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2020-05-19 17:17:46 -0300

    Create com112_sort.h

---
+++ lista5/com112_sort.h
@@ -0,0 +1,10 @@
+#ifndef COM_112SORT_H
+#define COM_112SORT_H
+
+void bubble_sort(int vetor[], int n);//método de ordenação bubble sort
+void selection_Sort(int vet[],int n);//método de ordenação selection sort
+void insertion_sort(int vetor[], int n);//método de ordenação insertion sort
+void merge(int *v, int inicio, int meio, int fim,int *comp,int (*mov));//Metodo merge do merge sort
+void mergeSort(int *V,int inicio,int fim,int *comp,int *mov);//Metodo merde sort
+
+#endif
