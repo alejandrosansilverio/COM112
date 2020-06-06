1fe71517326a6ded2486212f165f4e305a8eed09
alejandro-sansilverio <50168555+alejandro-sansilverio@users.noreply.github.com>
2020-06-06 12:58:21 -0300

    Update com112_sort.h

--- Atividade Avaliativa 1/com112_sort.h
+++ Atividade Avaliativa 1/com112_sort.h
@@ -1,5 +1,5 @@
-#ifndef COM_112SORT_H
-#define COM_112SORT_H
+#ifndef COM112_SORT_H
+#define COM112_SORT_H
 
 void bubble_sort(int vetor[], int n, int *comp, int *mov);//método de ordenação bubble sort
 void selection_Sort(int vet[],int n, int *comp, int *mov);//método de ordenação selection sort
